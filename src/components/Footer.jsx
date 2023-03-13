8d920a8864a665cc447f3ad36aabee5653f19f01
Saurabh <109129028+saurabh-sin@users.noreply.github.com>
2023-03-13 23:06:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,16 +11,16 @@ const Footer = () => {
     <div className={`-mt-5 pb-14 ${styles.padd} flex flex-wrap justify-center`}>
       
     <div className='flex flex-row flex-wrap justify-center gap-6'> 
-                <a href="https://github.com/saurabh-sin">
+                <a href="#">
                 <img src={github} alt='Github' className='w-10 h-10 object-contain'/>
                 </a>
-                <a href="mailto:sbhsingh6202@gmail.com">
+                <a href="#">
                 <img src={gmail} alt='Gmail' className='w-10 h-10 object-contain'/>
                 </a>
-                <a href="https://www.linkedin.com/in/saurabh-singh-3b6907195/">
+                <a href="#">
                 <img src={linkdin} alt='Linkdin' className='w-10 h-10 object-contain'/>
                 </a>
-                <a href="https://www.instagram.com/irascible_brain/">
+                <a href="#">
                 <img src={insta} alt='Instagram' className='w-10 h-10 object-contain'/>
                 </a>
     
